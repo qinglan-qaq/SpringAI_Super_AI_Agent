1e606019f4f087a8a99d2a12f1410c332075ea8e
qinglan-qaq <1710202731@qq.com>
2026-03-14 23:02:20 +0800

    新增如下

--- src/main/java/com/lx/aisuperagent/demo/invoke/LangchainAIInvoke.java
+++ src/main/java/com/lx/aisuperagent/demo/invoke/LangchainAIInvoke.java
@@ -1,4 +1,16 @@
 package com.lx.aisuperagent.demo.invoke;
 
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+
 public class LangchainAIInvoke {
+
+    public static void main(String[] args) {
+//        这个调用也很简洁
+        QwenChatModel qwenChatModel = QwenChatModel.builder()
+                .apiKey(TestApiKey.apiKey)
+                .modelName("qwen-plus")
+                .build();
+        String chat = qwenChatModel.chat("qwen-plus和qwen-max谁更好 区别在哪 我之前问max说自己最好");
+        System.out.println(chat);
+    }
 }
--- src/main/java/com/lx/aisuperagent/demo/invoke/SpringAIInvoke.java
+++ src/main/java/com/lx/aisuperagent/demo/invoke/SpringAIInvoke.java
@@ -1,4 +1,27 @@
 package com.lx.aisuperagent.demo.invoke;
 
-public class SpringAIInvoke {
+import jakarta.annotation.Resource;
+
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Spring ai 框架调用
+ */
+@Component
+public class SpringAIInvoke implements CommandLineRunner {
+
+    @Resource
+//    注意这里是按照名称引入的模型
+    private ChatModel dashscopeChatModel;
+
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage output = dashscopeChatModel.call(new Prompt("老婆~你终于回来了,桌上的菜还是热的,我们喝点小酒吗?(晃了晃酒杯)"))
+                .getResult().getOutput();
+        System.out.println(output.getText());
+    }
 }
