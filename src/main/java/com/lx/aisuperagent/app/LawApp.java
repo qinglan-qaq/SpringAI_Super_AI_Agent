9adb472ff556ccac77f368e486175472ab9a7354
qinglan-qaq <1710202731@qq.com>
2026-03-16 22:48:29 +0800

    如下

--- src/main/java/com/lx/aisuperagent/app/LawApp.java
+++ src/main/java/com/lx/aisuperagent/app/LawApp.java
@@ -1,3 +1,5 @@
+package com.lx.aisuperagent.app;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -11,16 +13,19 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class LawApp {
+    //    使用构造器初始化
     private final ChatClient chatClient;
 
-    public static final String SYSTEM_PROMPT= "";
+    public static final String SYSTEM_PROMPT = "你是一个专业的法律顾问AI，名为“AI私人法务”，精通中国法律体系。" +
+            "你的任务是为用户提供初步的法律咨询和建议，帮助他们理解自己的法律处境和可行的行动方案。" +
+            "在对话中，你需要通过引导性问题逐步深入了解用户的具体情况，模拟真实法律咨询场景。" +
+            "你的回答应当专业、清晰、易于理解，同时始终保持礼貌、耐心和同理心，让用户感受到被重视和支持。";
 
-    public LawApp(ChatModel dashscopeChatModel){
-
+    public LawApp(ChatModel dashscopeChatModel) {
 
         ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
-
-        ChatClient chatClient = ChatClient.builder(dashscopeChatModel)
+//       可以对全局启用预设 也可以对单次使用预设
+        this.chatClient = ChatClient.builder(dashscopeChatModel)
                 .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(
                         MessageChatMemoryAdvisor.builder(chatMemory).build()
@@ -28,16 +33,23 @@ public class LawApp {
                 .build();
     }
 
-    public String doChat(String message,String chatId){
+    /**
+     * Ai 对话支持对话记忆ChatMemory
+     *
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public String doChat(String message, String chatId) {
         ChatResponse response = chatClient
                 .prompt()
                 .user(message)
-                .advisors(advisorSpec -> advisorSpec.param(CHAT_MEMORY_CONVERSATION_ID_KEY,chatId)
-                .param(CHAT_MEMORY_CONVERSATION_ID_KEY,10))
+//                新版本写法
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, chatId))
                 .call()
                 .chatResponse();
         String content = response.getResult().getOutput().getText();
-        log.info("content:{}",content);
+        log.info("content:{}", content);
         return content;
     }
 }
--- src/test/java/com/lx/aisuperagent/app/LawAppTest.java
+++ src/test/java/com/lx/aisuperagent/app/LawAppTest.java
@@ -1,4 +1,12 @@
+package com.lx.aisuperagent.app;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LawAppTest {
-  
+
+    @Test
+    void doChat() {
+    }
 }
