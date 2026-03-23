3da5d232d71cc1309e18e34c0862f9655f0b1983
qinglan-qaq <1710202731@qq.com>
2026-03-23 10:17:56 +0800

    完成LawAppDocumentLoader代码编写 对Markdown文档的读取  需要改进的点: 将文档的description添加进元数据

--- src/main/java/com/lx/aisuperagent/rag/LawAppDocumentLoader.java
+++ src/main/java/com/lx/aisuperagent/rag/LawAppDocumentLoader.java
@@ -1,4 +1,56 @@
 package com.lx.aisuperagent.rag;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
+import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Slf4j
 public class LawAppDocumentLoader {
+    private final ResourcePatternResolver resourcePatternResolver;
+
+    //    构造器实现
+    LawAppDocumentLoader(ResourcePatternResolver resourcePatternResolver) {
+        this.resourcePatternResolver = resourcePatternResolver;
+    }
+
+    public List<Document> loadMarkdowns() {
+
+        List<Document> allDocuments = new ArrayList<>();
+
+        try {
+//           获取资源 从Document路径下获取所有md文件
+            Resource[] resources = resourcePatternResolver.getResources("classpath:document/*.md");
+            for (Resource resource : resources) {
+                String fileName = resource.getFilename();
+                String title = resource.getDescription();
+//                配置获取的参数
+                MarkdownDocumentReaderConfig config = MarkdownDocumentReaderConfig.builder()
+//                        新文档是否按照水平分割线划分
+                        .withHorizontalRuleCreateDocument(true)
+//                        是否包含代码块的内容
+                        .withIncludeCodeBlock(false)
+//                        是否包含引用块内容
+                        .withIncludeBlockquote(false)
+//                        将文件名作为metadata元数据
+                        .withAdditionalMetadata("filename", fileName)
+                        .build();
+                MarkdownDocumentReader markdownDocumentReader = new MarkdownDocumentReader(resource, config);
+                allDocuments.addAll(markdownDocumentReader.get());
+            }
+
+        } catch (Exception e) {
+            log.error("Markdown文档加载失败", e);
+        }
+
+        return allDocuments;
+    }
+
 }
