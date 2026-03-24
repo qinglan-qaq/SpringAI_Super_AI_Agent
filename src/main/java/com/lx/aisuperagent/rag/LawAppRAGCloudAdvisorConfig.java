f0cbc4600852772ccdfd9b9ac2e1dcff9b1e0659
qinglan-qaq <1710202731@qq.com>
2026-03-24 17:44:47 +0800

    新增文件云知识库的开发 对饮的配置文件

--- src/main/java/com/lx/aisuperagent/rag/LawAppRAGCloudAdvisorConfig.java
+++ src/main/java/com/lx/aisuperagent/rag/LawAppRAGCloudAdvisorConfig.java
@@ -1,4 +1,38 @@
 package com.lx.aisuperagent.rag;
 
-public class LawAppRAGCloudeAdvisorConfig {
+
+import com.alibaba.cloud.ai.dashscope.api.DashScopeAgentApi;
+import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import com.alibaba.cloud.ai.dashscope.rag.DashScopeDocumentRetrievalAdvisor;
+import com.alibaba.cloud.ai.dashscope.rag.DashScopeDocumentRetriever;
+import com.alibaba.cloud.ai.dashscope.rag.DashScopeDocumentRetrieverOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LawAppRAGCloudAdvisorConfig {
+
+    @Value("${spring.ai.dashscop.api.key}")
+    private String dashscopeApiKey;
+
+    @Bean
+    public Advisor lawAppRAGCloudAdvisor() {
+        DashScopeApi dashScopeApi = new DashScopeApi(dashscopeApiKey);
+
+        final String KNOWLEDGE_INDEX = "RAG云知识库";
+        DashScopeDocumentRetrievalAdvisor documentRetriever = new DashScopeDocumentRetriever(
+                dashScopeApi,
+                DashScopeDocumentRetrieverOptions.builder()
+                        .withIndexName(KNOWLEDGE_INDEX)
+                        .build());
+        return RetrievalAugmentationAdvisor.builder()
+                .documentRetriever(documentRetriever)
+                .build();
+    }
+
 }
--- src/main/java/com/lx/aisuperagent/rag/MyDocumentEnricher.java
+++ src/main/java/com/lx/aisuperagent/rag/MyDocumentEnricher.java
@@ -4,6 +4,8 @@ package com.lx.aisuperagent.rag;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.model.transformer.KeywordMetadataEnricher;
+import org.springframework.ai.model.transformer.SummaryMetadataEnricher;
+import org.springframework.ai.model.transformer.SummaryMetadataEnricher.SummaryType;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -17,11 +19,33 @@ public class MyDocumentEnricher {
         this.chatModel = chatModel;
     }
 
+    /**
+     * 提取关键字
+     * 关键字创建键值对,保存在元数据中
+     * 本质上是给AI添加额外的提示词生成
+     * @param documents
+     * @return
+     */
     List<Document> enrichDocumentsByKeyword(List<Document> documents) {
+
         KeywordMetadataEnricher enricher = KeywordMetadataEnricher
                 .builder(chatModel)
                 .keywordCount(5)
                 .build();
+        return enricher.apply(documents);
+    }
+
+    /**
+     * 摘要增强
+     * 生成文档摘要
+     * 生成考虑上下文的摘要,前者后者和当前文档
+     * 本质上是给AI添加额外的提示词生成
+     * @param documents
+     * @return
+     */
+    List<Document> enrichDocumentsBySummary(List<Document> documents) {
+        SummaryMetadataEnricher enricher = new SummaryMetadataEnricher(chatModel,
+                List.of(SummaryType.PREVIOUS, SummaryType.CURRENT, SummaryType.NEXT));
         return enricher.apply(documents);
     }
 }
