6b2b2777642ce30bada86678ba6f5e7c486476d7
qinglan-qaq <1710202731@qq.com>
2026-03-24 22:32:55 +0800

    完成对ToolCalling文件操作的代码编写

---
+++ src/test/java/com/lx/aisuperagent/Tools/FileOptionToolTest.java
@@ -0,0 +1,19 @@
+package com.lx.aisuperagent.Tools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileOptionToolTest {
+
+
+    @Test
+    void testReadFile() {
+
+    }
+
+    @Test
+    void testWriteFile() {
+
+    }
+}
--- src/test/java/com/lx/aisuperagent/rag/PgVectorVectorStoreConfigTest.java
+++ src/test/java/com/lx/aisuperagent/rag/PgVectorVectorStoreConfigTest.java
@@ -20,9 +20,11 @@ class PgVectorVectorStoreConfigTest {
     @Test
     void pgVectorVectorStoreTest() {
         List<Document> documents = List.of(
-                new Document("Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!!", Map.of("meta1", "meta1")),
+                new Document("Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!!",
+                        Map.of("meta1", "meta1")),
                 new Document("The World is Big and Salvation Lurks Around the Corner"),
-                new Document("You walk forward facing the past and you turn back toward the future.", Map.of("meta2", "meta2")));
+                new Document("You walk forward facing the past and you turn back toward the future.",
+                        Map.of("meta2", "meta2")));
         // 添加文档
         pgVectorVectorStore.add(documents);
         // 相似度查询
