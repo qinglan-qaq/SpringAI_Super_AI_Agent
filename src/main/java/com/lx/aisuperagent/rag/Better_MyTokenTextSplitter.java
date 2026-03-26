be66883b914b0b190c08a96a547f14c6c45e4009
qinglan-qaq <1710202731@qq.com>
2026-03-26 21:46:04 +0800

    更好的Builder模式的文档切分方式

---
+++ src/main/java/com/lx/aisuperagent/rag/Better_MyTokenTextSplitter.java
@@ -0,0 +1,27 @@
+package com.lx.aisuperagent.rag;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 使用Builder模式,提供了更加灵活的api接口
+ * 能自定义更多的文档分割细节
+ */
+@Component
+public class Better_MyTokenTextSplitter {
+    public List<Document> splitWithBuilder(List<Document> documents) {
+        TokenTextSplitter splitter = TokenTextSplitter.builder()
+                .withChunkSize(1000)
+                .withMinChunkSizeChars(400)
+                .withMinChunkLengthToEmbed(10)
+                .withMaxNumChunks(5000)
+                .withKeepSeparator(true)
+                .build();
+
+        return splitter.apply(documents);
+    }
+}
+
